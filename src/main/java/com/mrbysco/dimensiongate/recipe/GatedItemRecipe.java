d8e0eabf9e08c2dd4c23cdc4145737e65973805c
Mrbysco <Mrbysco@users.noreply.github.com>
2024-06-29 14:03:49 +0200

    Port to 1.21

--- src/main/java/com/mrbysco/dimensiongate/recipe/GatedItemRecipe.java
+++ src/main/java/com/mrbysco/dimensiongate/recipe/GatedItemRecipe.java
@@ -7,18 +7,17 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.mrbysco.dimensiongate.DimensionalItemGate;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
-import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.Level;
 import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.items.IItemHandler;
@@ -26,7 +25,7 @@ import net.neoforged.neoforge.items.IItemHandler;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GatedItemRecipe implements Recipe<Container> {
+public class GatedItemRecipe implements Recipe<SingleRecipeInput> {
 	protected final NonNullList<Ingredient> ingredients;
 	protected final ResourceKey<Level> dimension;
 	protected final ItemStack result = ItemStack.EMPTY;
@@ -49,7 +48,7 @@ public class GatedItemRecipe implements Recipe<Container> {
 	}
 
 	@Override
-	public boolean matches(Container container, Level level) {
+	public boolean matches(SingleRecipeInput input, Level level) {
 		//Unused please use getMatchingStacks instead as it will give you the stacks that match the recipe
 
 		return false;
@@ -89,8 +88,7 @@ public class GatedItemRecipe implements Recipe<Container> {
 		List<Ingredient> missingIngredients = new ArrayList<>(getIngredients());
 		if (!stacks.isEmpty()) {
 			missingIngredients.removeIf(ingredient -> {
-				if (stacks.stream().anyMatch(ingredient))
-					return true;
+				if (stacks.stream().anyMatch(ingredient)) return true;
 				for (ItemStack stack : stacks) {
 					IItemHandler handler = stack.getCapability(Capabilities.ItemHandler.ITEM);
 					if (handler != null) {
@@ -116,7 +114,7 @@ public class GatedItemRecipe implements Recipe<Container> {
 	}
 
 	@Override
-	public ItemStack assemble(Container container, HolderLookup.Provider provider) {
+	public ItemStack assemble(SingleRecipeInput input, HolderLookup.Provider provider) {
 		return getResultItem(provider).copy();
 	}
 
@@ -150,24 +148,15 @@ public class GatedItemRecipe implements Recipe<Container> {
 
 	public static class Serializer implements RecipeSerializer<GatedItemRecipe> {
 
-		private static final MapCodec<GatedItemRecipe> CODEC = RecordCodecBuilder.mapCodec(
-				instance -> instance.group(
-								Ingredient.CODEC_NONEMPTY.listOf().fieldOf("ingredients").flatXmap((array) -> {
-									Ingredient[] aingredient = array.toArray(Ingredient[]::new);
-									if (aingredient.length == 0) {
-										return DataResult.error(() -> "No items in Gated Item Recipe");
-									} else {
-										return DataResult.success(NonNullList.of(Ingredient.EMPTY, aingredient));
-									}
-								}, DataResult::success).forGetter(recipe -> recipe.ingredients),
-								Level.RESOURCE_KEY_CODEC.fieldOf("dimension").forGetter(recipe -> recipe.dimension),
-								Codec.BOOL.optionalFieldOf("required", false).forGetter(recipe -> recipe.required)
-						)
-						.apply(instance, GatedItemRecipe::new)
-		);
-		public static final StreamCodec<RegistryFriendlyByteBuf, GatedItemRecipe> STREAM_CODEC = StreamCodec.of(
-				GatedItemRecipe.Serializer::toNetwork, GatedItemRecipe.Serializer::fromNetwork
-		);
+		private static final MapCodec<GatedItemRecipe> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(Ingredient.CODEC_NONEMPTY.listOf().fieldOf("ingredients").flatXmap((array) -> {
+			Ingredient[] aingredient = array.toArray(Ingredient[]::new);
+			if (aingredient.length == 0) {
+				return DataResult.error(() -> "No items in Gated Item Recipe");
+			} else {
+				return DataResult.success(NonNullList.of(Ingredient.EMPTY, aingredient));
+			}
+		}, DataResult::success).forGetter(recipe -> recipe.ingredients), Level.RESOURCE_KEY_CODEC.fieldOf("dimension").forGetter(recipe -> recipe.dimension), Codec.BOOL.optionalFieldOf("required", false).forGetter(recipe -> recipe.required)).apply(instance, GatedItemRecipe::new));
+		public static final StreamCodec<RegistryFriendlyByteBuf, GatedItemRecipe> STREAM_CODEC = StreamCodec.of(GatedItemRecipe.Serializer::toNetwork, GatedItemRecipe.Serializer::fromNetwork);
 
 		@Override
 		public MapCodec<GatedItemRecipe> codec() {
